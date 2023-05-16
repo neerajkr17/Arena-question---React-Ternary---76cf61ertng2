c717c15bd72f490263afd9be72233f06ada63953
Neeraj Kumar <121371264+neerajkr17@users.noreply.github.com>
2023-05-16 19:45:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
+
 const App = () => {
-  const [day, setDay] = useState('')
-  const date = new Date()
+  const [day, setDay] = useState('');
+  const date = new Date();
+
   useEffect(() => {
     setDay(date.toLocaleString(window.navigator.language, { weekday: 'long' }));
-  }, [date])
-  const setTheMessage = (day) => {
-    return () ? "Hey Mango Monday" :
-     
+  }, [date]);
 
-  }
-  const msg = setTheMessage(day)
+  const setTheMessage = (day) => {
+    switch (day) {
+      case 'Monday':
+        return 'Hey Mango Monday';
+      case 'Tuesday':
+        return 'Hey Tomato Tuesday';
+      case 'Wednesday':
+        return 'Hey Windy Wednesday';
+      case 'Thursday':
+        return 'Hey Thunder Thursday';
+      case 'Friday':
+        return 'Hey Fun Friday';
+      case 'Saturday':
+        return 'Hey Smooth Saturday';
+      case 'Sunday':
+        return 'Hey Sugar Sunday';
+      default:
+        return 'unknown day';
+    }
+  };
+
+  const msg = setTheMessage(day);
 
   return (
     <div id="main">
-      <h1> {msg}</h1>
+      <h1>{msg}</h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
